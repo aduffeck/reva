e958992db63705e14363e3ba23eaa30875da74c6
André Duffeck <a.duffeck@opencloud.eu>
2025-12-05 10:31:18 +0100

    Add a logger

--- pkg/workqueue/options.go
+++ pkg/workqueue/options.go
@@ -1,13 +1,22 @@
 package workqueue
 
+import "github.com/rs/zerolog"
+
 type Option func(*options)
 
 type options struct {
 	storage Storage
+	log     *zerolog.Logger
 }
 
 func WithStorage(s Storage) Option {
 	return func(o *options) {
 		o.storage = s
+	}
+}
+
+func WithLogger(log *zerolog.Logger) Option {
+	return func(o *options) {
+		o.log = log
 	}
 }
--- pkg/workqueue/workqueue.go
+++ pkg/workqueue/workqueue.go
@@ -2,6 +2,7 @@ package workqueue
 
 import (
 	"github.com/opencloud-eu/reva/v2/pkg/workqueue/task"
+	"github.com/rs/zerolog"
 )
 
 type Storage interface {
@@ -12,6 +13,7 @@ type Storage interface {
 }
 type WorkQueue struct {
 	storage Storage
+	log     *zerolog.Logger
 }
 
 func New(opts ...Option) *WorkQueue {
@@ -20,8 +22,13 @@ func New(opts ...Option) *WorkQueue {
 		opt(&o)
 	}
 
+	if o.log == nil {
+		o.log = &zerolog.Logger{}
+	}
+
 	return &WorkQueue{
 		storage: o.storage,
+		log:     o.log,
 	}
 }
 
